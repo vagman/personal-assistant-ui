709d4a5c09dc80206df7d58545d8aaa8ac309e77
Kostas Gialantzis <kgialant@gmail.com>
2022-09-05 22:34:35 +0300

    initialized scheduler

---
+++ personal-assistant-ui/AddEvent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace personal_assistant_ui
+{
+    public partial class AddEvent : Form
+    {
+        public AddEvent()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "MM/dd/yyyy hh:mm:ss";
+            dateTimePicker1.ShowUpDown = true;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
