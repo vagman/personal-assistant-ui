8664a8dab960c09c9cf98d65638409df9832596c
vagman <bmanousakis@gmail.com>
2022-09-06 22:18:35 +0300

    Implemented login validation

--- personal-assistant-ui/login.Designer.cs
+++ personal-assistant-ui/login.Designer.cs
@@ -36,29 +36,30 @@
             this.label2 = new System.Windows.Forms.Label();
             this.tb_username = new System.Windows.Forms.TextBox();
             this.tb_password = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.loginErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.loginErrorProvider)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_login
             // 
             this.btn_login.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btn_login.Location = new System.Drawing.Point(355, 201);
+            this.btn_login.Location = new System.Drawing.Point(325, 193);
             this.btn_login.Name = "btn_login";
-            this.btn_login.Size = new System.Drawing.Size(95, 36);
+            this.btn_login.Size = new System.Drawing.Size(106, 39);
             this.btn_login.TabIndex = 0;
             this.btn_login.Text = "Login";
             this.btn_login.UseVisualStyleBackColor = true;
-            this.btn_login.Click += new System.EventHandler(this.button1_Click);
+            this.btn_login.Click += new System.EventHandler(this.login_Click);
             // 
             // btn_signup
             // 
             this.btn_signup.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btn_signup.Location = new System.Drawing.Point(409, 279);
+            this.btn_signup.Location = new System.Drawing.Point(407, 312);
             this.btn_signup.Name = "btn_signup";
-            this.btn_signup.Size = new System.Drawing.Size(59, 34);
+            this.btn_signup.Size = new System.Drawing.Size(80, 29);
             this.btn_signup.TabIndex = 1;
             this.btn_signup.Text = "here";
             this.btn_signup.UseVisualStyleBackColor = true;
@@ -85,28 +86,23 @@
             // tb_username
             // 
             this.tb_username.Location = new System.Drawing.Point(325, 117);
+            this.tb_username.MaxLength = 30;
             this.tb_username.Name = "tb_username";
             this.tb_username.PlaceholderText = "Enter your username";
             this.tb_username.Size = new System.Drawing.Size(180, 23);
             this.tb_username.TabIndex = 4;
+            this.tb_username.Validating += new System.ComponentModel.CancelEventHandler(this.tb_username_Validating);
             // 
             // tb_password
             // 
-            this.tb_password.Location = new System.Drawing.Point(324, 150);
+            this.tb_password.Location = new System.Drawing.Point(325, 150);
+            this.tb_password.MaxLength = 14;
             this.tb_password.Name = "tb_password";
+            this.tb_password.PasswordChar = '*';
             this.tb_password.PlaceholderText = "Enter your password";
             this.tb_password.Size = new System.Drawing.Size(180, 23);
             this.tb_password.TabIndex = 5;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 407);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(266, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "MainMenu testButton (TO BE DELETED)";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.tb_password.Validating += new System.ComponentModel.CancelEventHandler(this.tb_password_Validating);
             // 
             // button4
             // 
@@ -123,7 +119,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(296, 289);
+            this.label3.Location = new System.Drawing.Point(294, 319);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(107, 30);
             this.label3.TabIndex = 8;
@@ -134,15 +130,21 @@
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
+            // loginErrorProvider
+            // 
+            this.loginErrorProvider.BlinkRate = 0;
+            this.loginErrorProvider.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.loginErrorProvider.ContainerControl = this;
+            // 
             // login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
             this.BackColor = System.Drawing.Color.LightBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.tb_password);
             this.Controls.Add(this.tb_username);
             this.Controls.Add(this.label2);
@@ -152,7 +154,9 @@
             this.Name = "login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Personal Assistant - Login";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.login_FormClosing);
             this.Load += new System.EventHandler(this.login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.loginErrorProvider)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -166,9 +170,9 @@
         private Label label2;
         private TextBox tb_username;
         private TextBox tb_password;
-        private Button button1;
         private Button button4;
         private Label label3;
         private ContextMenuStrip contextMenuStrip1;
+        private ErrorProvider loginErrorProvider;
     }
 }
